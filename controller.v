b3f5705510abe73beba06598774f7f21118f5842
bkchan96 <39748671+bkchan96@users.noreply.github.com>
2018-11-07 19:27:24 -0800

    A1.3
    - Added reset routine
    - Added LFSR
    - Added some framework to display.v
    - Added blank .xdc file

--- controller.v
+++ controller.v
@@ -1,19 +1,15 @@
 `timescale 1ns / 1ps
 
-module controller(left, right, up, down, enter, game_reset);
+module controller(clk, reset, left, right, up, down, enter, game_reset);
+    input clk, reset;
     input left, right, up, down, enter, game_reset;
     
-    wire trigger = left || right || up || down || enter || game_reset;
+    wire trigger = left || right || up || down || enter;
     
     reg [2:0] board [7:0][7:0];
     
-    reg [2:0] i, k;
-    
     always @(posedge trigger) begin
-        if (game_reset) begin
-            // reset board
-        end
-        else if (enter) begin
+        if (enter) begin
             // enter key is pressed to select thing
         end
         else if (left) begin
@@ -27,6 +23,60 @@ module controller(left, right, up, down, enter, game_reset);
         end
         else if (down) begin
             // move down
+        end
+    end
+    
+    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Reset Routine
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    // declare wire and subtract by one because LFSR cannot output 0
+    wire [2:0] rout, random;
+    assign rout = random - 1;
+    
+    // instantiate random number generator
+    LFSR u_LFSR(.clk(clk), .random(random));
+    
+    // declare counter to count up to 64
+    reg [2:0] counter1, counter2;
+    
+    // declare register to store state
+    reg state;
+    
+    // reset routine
+    always @(posedge clk, posedge game_reset) begin
+        if (reset) begin
+            counter1 <= 0;
+            counter2 <= 0;
+            state <= 0;
+        end
+        if (game_reset) begin
+            counter1 <= 0;
+            counter2 <= 0;
+            state <= 1;
+        end
+        if (state == 1) begin
+            // reset complete if both counters are 7
+            if (counter1 == 7 && counter2 == 7)
+                state <= 0;
+            
+            // start reseting the board
+            else begin
+                // if random number is 5, throw away and do it on the next clock cycle
+                if (rout < 5) begin
+                    board[counter1][counter2] <= rout;
+                    
+                    // increment counters to run through all board spaces
+                    if (counter1 == 7) begin
+                        counter1 <= 0;
+                        counter2 <= counter2 + 1;
+                    end
+                    else begin
+                        counter1 = counter1 + 1;
+                    end
+                end
+            end
         end
     end
 
--- display.v
+++ display.v
@@ -1,6 +1,20 @@
 `timescale 1ns / 1ps
 
-module display();
-
+module display(video_on, pix_x, pix_y, graph_rgb);
+    input video_on;
+    input [9:0] pix_x, pix_y;
+    output reg [11:0] graph_rgb;
+    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    // rgb multiplexing circuit
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    localparam WHITE    = 12'b111111111111;
+    
+    always @*
+        if (video_on)
+            graph_rgb = 0; // blank
+        else
+            graph_rgb = WHITE;
 
 endmodule
