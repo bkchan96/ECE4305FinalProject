b101ba03fa497ad23247890550485ed110992d77
bkchan96 <39748671+bkchan96@users.noreply.github.com>
2018-11-07 17:03:48 -0800

    A1.1
    - VGA Framework Setup
    - (fix) tied ps2 to inputs in top.v
    - created display.v (not developed)
    - created controller.v (some inputs specified)

--- top.v
+++ top.v
@@ -1,7 +1,10 @@
 `timescale 1ns / 1ps
 
-module top(clk, reset);
-    input clk, reset;
+module top(clk, reset, dreset, ps2d, ps2c, hsync, vsync, rgb);
+    input clk, reset, dreset;
+    input ps2d, ps2c;
+    output hsync, vsync;
+    output [11:0] rgb;
     
     //////////////////////////////////////////////////////////////////////////////////////////////////////
     // Keyboard section
@@ -12,7 +15,7 @@ module top(clk, reset);
     wire key_left, key_right, key_up, key_down, key_game_reset, key_enter;
     
     // instantiate ps2 module
-    ps2_rx u_ps2_rx(.clk(clk), .reset(reset), .ps2d(), .ps2c(), .rx_en(), .rx_done_tick(rx_done_tick), .dout(scan_code));
+    ps2_rx u_ps2_rx(.clk(clk), .reset(reset), .ps2d(ps2d), .ps2c(ps2c), .rx_en(1'b1), .rx_done_tick(rx_done_tick), .dout(scan_code));
     
     // instantiate 
     kb_controller kb_left(.clk(clk), .reset(reset), .scan_done_tick(rx_done_tick), .scan_code(scan_code),
@@ -38,5 +41,26 @@ module top(clk, reset);
     kb_controller kb_enter(.clk(clk), .reset(reset), .scan_done_tick(rx_done_tick), .scan_code(scan_code),
         .scan_code_read(8'h5A),
         .key(key_enter));
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    // VGA Output Section
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    // signal declaration
+    wire [9:0] pixel_x, pixel_y;
+    wire video_on, pixel_tick;
+    reg [11:0] rgb_reg;
+    wire [11:0] rgb_next;
+    
+    vga_sync u_vga_sync(.clk(clk), .reset(~dreset), .hsync(hsync), .vsync(vsync),
+        .video_on(video_on), .p_tick(pixel_tick), .pixel_x(pixel_x), .pixel_y(pixel_y));
+    
+    // assign output
+    assign rgb = rgb_reg;
+    
+    // rgb buffer
+    always @(posedge clk)
+        if (pixel_tick)
+            rgb_reg <= rgb_next;
 
 endmodule
